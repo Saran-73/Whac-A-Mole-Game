ff93b28cbe2497dff87db69060f2c23ef459bb22
Saran-73 <saransamikannu@gmail.com>
2022-05-05 11:52:59 +0530

    mole appears every half second and clicking it will add score

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const score=document.querySelector(".score")
-let timeLeft=document.querySelector(".time-left")
+const timeLeft=document.querySelector(".time-left")
 const mole=document.querySelectorAll(".mole")
 const box=document.querySelectorAll(".box")
 
 let hitid=null;
+let totalScore=0;
 
-//remove the presence of class mole
+//initially remove the presence of any class mole 
 function clearMole(){
 mole.forEach((x)=>{
     x.classList.remove("mole")
 })
 }
-//make mole apppear randomly
+
+//make the mole apppear randomly
 function moleAppear(){
   let molelocation=box[Math.floor(Math.random() * 9)];
     molelocation.classList.add("mole")
+    //id of mole location is set to hitid variable
     hitid=molelocation.id
+    //mole is removed after 0.5s which will be the start of new mole appearence
+    setTimeout(() => {
+    molelocation.classList.remove("mole")
+    }, 500);
 }
 
+//mole appears every 0.5seconds by using setInterval
+//  setInterval(moleAppear,500)
+
+ //each box is given eventlistener when clicked look for the match of box id that is clicked and id of mole location
+ //when the match is true add score by 10 and display it
 box.forEach(y=>{
-//     y.addEventListener('mouseup',)
-//     if(y.id=== hitid){
-
-//     }
-
+    y.addEventListener('mouseup',function lookmatch(){
+      if(y.id == hitid){
+        totalScore = totalScore + 10;
+        score.textContent=totalScore;
+      }
+})
 })
  
 
